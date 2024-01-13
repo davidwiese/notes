943ea7dafd113180df78e5105abfd3d459f5d6aa
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-01-13 13:19:47 -0800

    Update firebase settings

--- firebase.js
+++ firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore";
 
 const firebaseConfig = {
 	apiKey: process.env.apiKey,
@@ -10,3 +11,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const notesCollection = collection(db, "notes");
